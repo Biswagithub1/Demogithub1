85d877587b222ba0030c22da496f2cef9dba45cd
Biswa <Biswa@Biswa-HP>
2024-08-12 11:21:04 +0530

    2 push

---
+++ Demogithub1/src/test/java/contactTest1/toCreateContact.java
@@ -0,0 +1,10 @@
+package contactTest1;
+
+public class toCreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("contact created for github1");
+
+	}
+
+}
---
+++ Demogithub1/src/test/java/orgTest1/toCreateOrg.java
@@ -0,0 +1,10 @@
+package orgTest1;
+
+public class toCreateOrg {
+
+	public static void main(String[] args) {
+		System.out.println("org created for github1");
+
+	}
+
+}
